2f685fb6f00f1e0bbc88206788189d26d2960ffd
ankitrmahale <ankitraghumahale@gmail.com>
2016-02-20 22:36:50 -0800

    Shell forked

--- shell.c
+++ shell.c
@@ -1,5 +1,9 @@
 #include  <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include  <sys/types.h>
+#include  <fcntl.h>
+#include  <unistd.h>
 
 void  split(char *commandline, char **splitter)
 {
@@ -13,24 +17,84 @@ void  split(char *commandline, char **splitter)
      *splitter = '\0';                 
 }
 
-void  execute(char **splitter)
+void  ChildProcess(char **splitter)
 {
+     
      execvp(*splitter, splitter);
+     
+     
 }
 
-void  main(void)
+void  ParentProcess(char **splitter)
+{
+     //execvp(*splitter, splitter);
+     
+}
+
+
+
+void execute_parent(char **splitter)
+{
+}    
+
+int main(int argc, char **argv)
 {
      char  commandline[500];             
-     char  *splitter[50];              
-
-     while (1) {                   
-          printf("Shell -> ");     
+     char  *splitter[50]; 
+     pid_t  pid;
+     int status;             
+     
+      while(1)              
+      {    printf("\nShell -> ");     
           gets(commandline);              
           printf("\n");
-          split(commandline, splitter);       
+          
+          split(commandline, splitter); 
           if (strcmp(splitter[0], "exit") == 0)  
-               exit(0);                            
-          execute(splitter);           
-     }
+               exit(0); 
+            
+               
+                  
+                  pid = fork();
+                            if (pid == -1)
+                  {
+                      // error, failed to fork()
+                  } 
+                  
+                   if (pid == 0)
+                  {
+                     
+                      if (strcmp(splitter[0], "ls") == 0)  
+                      {
+                        int file=open(splitter[2],O_WRONLY);
+                        dup2(file,1);
+                        execvp("ls", argv);
+                      }
+                      if(!strcmp(splitter[0],"clear") || !strcmp(splitter[0],"cc") || !strcmp(splitter[0],"mkdir") || !strcmp(splitter[0],"rmdir") || !strcmp(splitter[0],"rm"))
+                      {
+                        execvp(splitter[0],splitter);
+                      }
+                      _exit(EXIT_FAILURE);   
+
+                  
+                }
+                  else 
+                  {
+                      int status;
+                     
+                      waitpid(pid, &status, 0);
+                      
+                         
+                  } 
+
+          
+
+
+           
+      }       
+                                     
+                     
+     
+    
 }
 //
